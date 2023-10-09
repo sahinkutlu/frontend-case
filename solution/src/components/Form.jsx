8b3139ef8d287dc517e1a8e76778e4ab2e8763f0
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-10-09 10:11:14 +0300

    Add category select handle function

--- solution/src/components/Form.jsx
+++ solution/src/components/Form.jsx
@@ -12,14 +12,42 @@ categoryArray.forEach((categoryName) => {
   const obj = {
     id: uuidv4(),
     name: categoryName,
-    checked: false,
+    isChecked: false,
   };
   categoryExtend.push(obj);
 });
-console.log(categoryExtend);
 
 const Form = () => {
   const [category, setCategory] = useState(categoryExtend);
+
+  const handleCategoryClick = (e) => {
+    // Making copy of state
+    const array = [...category];
+    if (e.target.checked) {
+      array.forEach((item) => {
+        if (item.name === e.target.name) {
+          // Making a copy of item and than deleting item from array
+          const index = array.indexOf(item);
+          const element = array[index];
+          array.splice(index, 1);
+
+          // Set element's isChecked value to true
+          element.isChecked = true;
+
+          // Add element to beginning of array
+          array.unshift(element);
+        }
+        setCategory(array);
+      });
+    } else {
+      array.forEach((item) => {
+        if (item.name === e.target.name) {
+          item.isChecked = false;
+        }
+      });
+      setCategory(array);
+    }
+  };
   return (
     <form action="">
       <h2>Kategoriler</h2>
@@ -32,7 +60,7 @@ const Form = () => {
           name="search"
         />
       </div>
-      <Select category={category} />
+      <Select category={category} handleCategoryClick={handleCategoryClick} />
       <button>Ara</button>
     </form>
   );
--- solution/src/components/Select.jsx
+++ solution/src/components/Select.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 const Select = (props) => {
-  const { category } = props;
+  const { category, handleCategoryClick } = props;
 
   return (
-    <div>
+    <div className="categories">
       {category.map((item) => {
         return (
-          <div key={item.id}>
-            <input name={item.name} type="checkbox" checked={item.checked} />
+          <div key={item.id} className="category">
+            <input
+              name={item.name}
+              type="checkbox"
+              defaultChecked={item.isChecked}
+              className="category-input"
+              onClick={handleCategoryClick}
+            />
             <label htmlFor={item.name}>{item.name}</label>
           </div>
         );
