2eab98c53efc5491aec8efecf9de193abc242069
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-10-09 08:36:58 +0300

    Add categories to form

--- solution/src/components/Form.jsx
+++ solution/src/components/Form.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import Select from "./Select";
+import itemsJson from "../../../assets/items";
+
+const categoryArray = itemsJson.data;
+
+// Making a new array of objects from categoryArray
+let categoryExtend = [];
+categoryArray.forEach((categoryName) => {
+  const obj = {
+    id: uuidv4(),
+    name: categoryName,
+    checked: false,
+  };
+  categoryExtend.push(obj);
+});
+console.log(categoryExtend);
 
 const Form = () => {
+  const [category, setCategory] = useState(categoryExtend);
   return (
     <form action="">
       <h2>Kategoriler</h2>
@@ -13,6 +32,8 @@ const Form = () => {
           name="search"
         />
       </div>
+      <Select category={category} />
+      <button>Ara</button>
     </form>
   );
 };
--- solution/src/components/Select.jsx
+++ solution/src/components/Select.jsx
@@ -1,1 +1,20 @@
 import React from "react";
+
+const Select = (props) => {
+  const { category } = props;
+
+  return (
+    <div>
+      {category.map((item) => {
+        return (
+          <div key={item.id}>
+            <input name={item.name} type="checkbox" checked={item.checked} />
+            <label htmlFor={item.name}>{item.name}</label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Select;
