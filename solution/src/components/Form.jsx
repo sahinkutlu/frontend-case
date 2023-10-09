5973a82f7bfb6ed4e8253c0a9d7f0aedaece7865
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-10-10 01:49:36 +0300

    Add error handling

--- solution/src/components/Form.jsx
+++ solution/src/components/Form.jsx
@@ -26,130 +26,152 @@ const Form = () => {
 
   const [category, setCategory] = useState(localCategory || categoryExtend);
   const [search, setSearch] = useState([]);
+  const [error, setError] = useState(null);
 
   const handleCategoryClick = (e) => {
-    // Making copy of state
-    const array = [...category];
-    if (e.target.checked) {
-      array.forEach((item) => {
-        if (item.name === e.target.name) {
-          item.isChecked = true;
-        }
-      });
-    } else {
-      array.forEach((item) => {
-        if (item.name === e.target.name) {
-          item.isChecked = false;
-        }
-      });
+    try {
+      // Making copy of state
+      const array = [...category];
+      if (e.target.checked) {
+        array.forEach((item) => {
+          if (item.name === e.target.name) {
+            item.isChecked = true;
+          }
+        });
+      } else {
+        array.forEach((item) => {
+          if (item.name === e.target.name) {
+            item.isChecked = false;
+          }
+        });
+      }
+      array.sort((a, b) => b.isChecked - a.isChecked);
+      setCategory(array);
+      // Saving data to prevent data loss on refresh
+      localStorage.setItem("category", JSON.stringify(category));
+    } catch (err) {
+      setError(err);
     }
-    array.sort((a, b) => b.isChecked - a.isChecked);
-    setCategory(array);
-    // Saving data to prevent data loss on refresh
-    localStorage.setItem("category", JSON.stringify(category));
   };
 
   const handleSearchClick = (e) => {
-    // Making copy of states
-    const searchArr = [...search];
-    const categoryArr = [...category];
-    if (e.target.checked) {
-      searchArr.forEach((item) => {
-        if (item.name === e.target.name) {
-          // Making a copy of item and than deleting item from array
-          const index = searchArr.indexOf(item);
-          const element = searchArr[index];
-          searchArr.splice(index, 1);
+    try {
+      // Making copy of states
+      const searchArr = [...search];
+      const categoryArr = [...category];
+      if (e.target.checked) {
+        searchArr.forEach((item) => {
+          if (item.name === e.target.name) {
+            // Making a copy of item and than deleting item from array
+            const index = searchArr.indexOf(item);
+            const element = searchArr[index];
+            searchArr.splice(index, 1);
 
-          // Set element's isChecked value to true
-          element.isChecked = true;
+            // Set element's isChecked value to true
+            element.isChecked = true;
 
-          // Add element to beginning of array
-          searchArr.unshift(element);
+            // Add element to beginning of array
+            searchArr.unshift(element);
 
-          // Changing element's isChecked status on category state
-          categoryArr.forEach((item) => {
-            if (item.id === element.id) {
-              const index = categoryArr.indexOf(item);
-              // Deleting item from categoryArr
-              categoryArr.splice(index, 1);
-              // Adding element to begining of the categoryArr
-              categoryArr.unshift(element);
-            }
-          });
-        }
+            // Changing element's isChecked status on category state
+            categoryArr.forEach((item) => {
+              if (item.id === element.id) {
+                const index = categoryArr.indexOf(item);
+                // Deleting item from categoryArr
+                categoryArr.splice(index, 1);
+                // Adding element to begining of the categoryArr
+                categoryArr.unshift(element);
+              }
+            });
+          }
+          setSearch(searchArr);
+          setCategory(categoryArr);
+        });
+      } else {
+        searchArr.forEach((item) => {
+          if (item.name === e.target.name) {
+            item.isChecked = false;
+          }
+        });
+        categoryArr.forEach((item) => {
+          if (item.name === e.target.name) {
+            item.isChecked = false;
+          }
+        });
         setSearch(searchArr);
         setCategory(categoryArr);
-      });
-    } else {
-      searchArr.forEach((item) => {
-        if (item.name === e.target.name) {
-          item.isChecked = false;
-        }
-      });
-      categoryArr.forEach((item) => {
-        if (item.name === e.target.name) {
-          item.isChecked = false;
-        }
-      });
-      setSearch(searchArr);
-      setCategory(categoryArr);
+      }
+      // Saving data to prevent data loss on refresh
+      localStorage.setItem("category", JSON.stringify(category));
+    } catch (err) {
+      setError(err);
     }
-    // Saving data to prevent data loss on refresh
-    localStorage.setItem("category", JSON.stringify(category));
   };
 
   const handleChange = (e) => {
-    // If user delete all search str set search as an empty array
-    if (e.target.value === "") {
-      setSearch([]);
-      return;
+    try {
+      // If user delete all search str set search as an empty array
+      if (e.target.value === "") {
+        setSearch([]);
+        return;
+      }
+
+      let array = [];
+
+      // Getting checked catagories from category state
+      let categoryArr = [...category];
+      let checkedCategories = 0;
+      categoryArr.forEach((item) => {
+        if (item.isChecked) {
+          array.push(item);
+          checkedCategories++;
+        }
+      });
+      // Delete checked catagories from categoryArr
+      categoryArr.splice(0, checkedCategories);
+
+      categoryArr.forEach((item) => {
+        if (item.name.toLowerCase().startsWith(e.target.value.toLowerCase())) {
+          array.push(item);
+        }
+      });
+
+      setSearch(array);
+    } catch (err) {
+      setError(err);
     }
-
-    let array = [];
-
-    // Getting checked catagories from category state
-    let categoryArr = [...category];
-    let checkedCategories = 0;
-    categoryArr.forEach((item) => {
-      if (item.isChecked) {
-        array.push(item);
-        checkedCategories++;
-      }
-    });
-    // Delete checked catagories from categoryArr
-    categoryArr.splice(0, checkedCategories);
-
-    categoryArr.forEach((item) => {
-      if (item.name.toLowerCase().startsWith(e.target.value.toLowerCase())) {
-        array.push(item);
-      }
-    });
-
-    setSearch(array);
   };
 
   return (
-    <form action="">
-      <h2>Kategoriler</h2>
-      <div className="search">
-        <label htmlFor="search"></label>
-        <input
-          type="text"
-          id="search"
-          placeholder="Kategori ara..."
-          name="search"
-          onChange={handleChange}
-        />
-      </div>
-      <Select
-        category={category}
-        handleCategoryClick={handleCategoryClick}
-        search={search}
-        handleSearchClick={handleSearchClick}
-      />
-      <button>Ara</button>
-    </form>
+    <>
+      {error ? (
+        <>
+          <h2>Sorry, an unexpected error occurred!!!</h2>
+          <p>{error}</p>
+        </>
+      ) : (
+        <form action="">
+          <h2>Kategoriler</h2>
+          <div className="search">
+            <label htmlFor="search"></label>
+            <input
+              type="text"
+              id="search"
+              placeholder="Kategori ara..."
+              name="search"
+              onChange={handleChange}
+            />
+          </div>
+          <Select
+            category={category}
+            handleCategoryClick={handleCategoryClick}
+            search={search}
+            handleSearchClick={handleSearchClick}
+          />
+          <button>Ara</button>
+        </form>
+      )}
+    </>
   );
 };
 
