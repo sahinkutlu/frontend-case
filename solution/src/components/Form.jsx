3ab83b5f3aeb40724115662491f075c1042132ef
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-10-10 00:02:31 +0300

    Add handle category search functionality

--- solution/src/components/Form.jsx
+++ solution/src/components/Form.jsx
@@ -19,6 +19,7 @@ categoryArray.forEach((categoryName) => {
 
 const Form = () => {
   const [category, setCategory] = useState(categoryExtend);
+  const [search, setSearch] = useState([]);
 
   const handleCategoryClick = (e) => {
     // Making copy of state
@@ -48,6 +49,88 @@ const Form = () => {
       setCategory(array);
     }
   };
+
+  const handleSearchClick = (e) => {
+    // Making copy of states
+    const searchArr = [...search];
+    const categoryArr = [...category];
+    if (e.target.checked) {
+      searchArr.forEach((item) => {
+        if (item.name === e.target.name) {
+          // Making a copy of item and than deleting item from array
+          const index = searchArr.indexOf(item);
+          const element = searchArr[index];
+          searchArr.splice(index, 1);
+
+          // Set element's isChecked value to true
+          element.isChecked = true;
+
+          // Add element to beginning of array
+          searchArr.unshift(element);
+
+          // Changing element's isChecked status on category state
+          categoryArr.forEach((item) => {
+            if (item.id === element.id) {
+              // Making copy of state
+              const index = categoryArr.indexOf(item);
+              // Deleting item from categoryArr
+              categoryArr.splice(index, 1);
+              // Adding element to begining of the categoryArr
+              categoryArr.unshift(element);
+            }
+          });
+        }
+        setSearch(searchArr);
+        setCategory(categoryArr);
+      });
+    } else {
+      searchArr.forEach((item) => {
+        if (item.name === e.target.name) {
+          item.isChecked = false;
+        }
+      });
+      categoryArr.forEach((item) => {
+        if (item.name === e.target.name) {
+          item.isChecked = false;
+        }
+      });
+      setSearch(searchArr);
+      setCategory(categoryArr);
+    }
+  };
+
+  const handleChange = (e) => {
+    // If user delete all search str set search as an empty array
+    if (e.target.value === "") {
+      setSearch([]);
+      return;
+    }
+
+    let array = [];
+
+    // Getting checked catagories from category state
+    let categoryArr = [...category];
+    let i = 0;
+    categoryArr.forEach((item) => {
+      if (item.isChecked) {
+        array.push(item);
+        i++;
+      }
+    });
+    // Delete checked catagories from categoryArr
+    categoryArr.splice(0, i);
+
+    categoryArr.forEach((item) => {
+      if (item.name.toLowerCase().startsWith(e.target.value.toLowerCase())) {
+        array.push(item);
+        const index = categoryArr.indexOf(item);
+        categoryArr.splice(index, 1);
+      }
+    });
+
+    setSearch(array);
+  };
+
   return (
     <form action="">
       <h2>Kategoriler</h2>
@@ -58,9 +141,15 @@ const Form = () => {
           id="search"
           placeholder="Kategori ara..."
           name="search"
+          onChange={handleChange}
         />
       </div>
-      <Select category={category} handleCategoryClick={handleCategoryClick} />
+      <Select
+        category={category}
+        handleCategoryClick={handleCategoryClick}
+        search={search}
+        handleSearchClick={handleSearchClick}
+      />
       <button>Ara</button>
     </form>
   );
--- solution/src/components/Select.jsx
+++ solution/src/components/Select.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 
 const Select = (props) => {
-  const { category, handleCategoryClick } = props;
+  const { category, handleCategoryClick, search, handleSearchClick } = props;
 
   return (
     <div className="categories">
-      {category.map((item) => {
-        return (
-          <div key={item.id} className="category">
-            <input
-              name={item.name}
-              type="checkbox"
-              defaultChecked={item.isChecked}
-              className="category-input"
-              onClick={handleCategoryClick}
-            />
-            <label htmlFor={item.name}>{item.name}</label>
-          </div>
-        );
-      })}
+      {search.length
+        ? search.map((item) => {
+            return (
+              <div key={item.id} className="category" id="s">
+                <input
+                  name={item.name}
+                  type="checkbox"
+                  defaultChecked={item.isChecked}
+                  className="category-input"
+                  onClick={handleSearchClick}
+                />
+                <label htmlFor={item.name}>{item.name}</label>
+              </div>
+            );
+          })
+        : category.map((item) => {
+            return (
+              <div key={item.id} className="category" id="c">
+                <input
+                  name={item.name}
+                  type="checkbox"
+                  defaultChecked={item.isChecked}
+                  className="category-input"
+                  onClick={handleCategoryClick}
+                />
+                <label htmlFor={item.name}>{item.name}</label>
+              </div>
+            );
+          })}
     </div>
   );
 };
