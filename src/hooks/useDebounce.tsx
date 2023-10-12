de36b0804fb59ea8ff00840652917cb387275bc6
Batuhan Ozen <batuhan@simplisales.com>
2023-10-12 23:45:23 +0300

    create useDebounce hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = <T extends unknown>(initialValue: T, timeOutInMs = 500): T => {
+  const [value, setValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setValue(initialValue);
+    }, timeOutInMs);
+
+    // Clear the timer if the effect re-runs before the timeout
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [initialValue, timeOutInMs]);
+
+  return value;
+};
+export default useDebounce;
+export { useDebounce };
