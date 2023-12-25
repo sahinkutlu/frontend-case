24b6d8658ea5fbd1fc699add1b76bc38aa996c48
Gökhan Duman <gokhand@forwardie.com>
2023-12-25 22:22:18 +0300

    chore:test commit for husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
     return (
         <>
-            <h1 className="text-2xl italic text-teal-400">Hello TailwindCSS</h1>
+            <h1 className="text-2xl italic text-teal-400 ">
+                Hello TailwindCSS
+            </h1>
         </>
     );
 }
