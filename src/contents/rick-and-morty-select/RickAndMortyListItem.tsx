f0941cb8658baef04bdf4b3bdb74dd1fac6a8a7a
gkhanduman <c2@gkhan.me>
2024-01-03 23:30:54 +0300

    feat: added check icon to selected list items

--- src/contents/rick-and-morty-select/RickAndMortyListItem.tsx
+++ src/contents/rick-and-morty-select/RickAndMortyListItem.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import get from "lodash/get";
 
+import { CheckIcon } from "@heroicons/react/20/solid";
+
 import HighlightFilterText from "@/components/text/HighlightFilterText";
 
 import { CharacterProps } from ".";
@@ -30,19 +32,24 @@ const RickAndMortyListItem: React.FC<RickAndMortyListItemProps> = ({
             : `${episodes.length} Episode`;
 
     return (
-        <div className="group relative grid cursor-default auto-cols-max grid-flow-col grid-rows-2 gap-x-3 overflow-hidden rounded px-2 py-1 text-slate-700">
+        <div className="group relative flex items-center gap-3 overflow-hidden px-2 py-1">
             <img
                 src={imageSrc}
                 alt={name}
-                className="row-span-2 h-8 w-8 place-self-center rounded-full"
+                className="h-8 w-8 flex-shrink-0 rounded-full"
             />
-            <div slot="label" className="truncate font-medium">
-                <HighlightFilterText text={name} filterText={filterText} />
+            <div className="flex flex-1 flex-col">
+                <div className="truncate font-medium">
+                    <HighlightFilterText text={name} filterText={filterText} />
+                </div>
+                <div className="truncate text-sm text-slate-600">
+                    {episodeLengthText}
+                </div>
             </div>
-            <div slot="description" className="truncate text-sm text-slate-600">
-                {episodeLengthText}
+            <div className="flex-shrink-0 opacity-0 group-selected:opacity-100">
+                <CheckIcon className="w-4 text-green-400" />
             </div>
-            <div className="absolute bottom-0 left-12 right-2 h-px bg-slate-200 group-selected:bg-slate-100 [.group:not([data-selected]):has(+[data-selected])_&]:hidden [.group[data-selected]:has(+:not([data-selected]))_&]:hidden [.group[data-selected]:last-child_&]:hidden" />
+            <div className="absolute bottom-0 left-12 right-2 h-px bg-slate-200 group-selected:bg-white [.group:not([data-selected]):has(+[data-selected])_&]:hidden [.group[data-selected]:has(+:not([data-selected]))_&]:hidden [.group[data-selected]:last-child_&]:hidden" />
         </div>
     );
 };
